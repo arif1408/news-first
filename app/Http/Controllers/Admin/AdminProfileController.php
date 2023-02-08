e92f94b975114809ec27ab3bffe54c6a283e698e
imam <maarifbatubara@gmail.com>
2023-02-08 15:02:50 +0700

    edit profile until send data as name

---
+++ app/Http/Controllers/Admin/AdminProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminProfileController extends Controller
+{
+    public function index()
+    {
+        return view('admin.profile');
+    }
+
+    public function profile_submit(Request $request)
+    {
+        echo $request->name;
+    }
+}
