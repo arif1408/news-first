b1861b0c55d57013ca77c200a29287e88af86a9b
imam <maarifbatubara@gmail.com>
2023-02-15 11:36:02 +0700

    make post comand for top advertisement

--- app/Http/Controllers/Admin/AdminAdvertisementController.php
+++ app/Http/Controllers/Admin/AdminAdvertisementController.php
@@ -65,4 +65,31 @@ class AdminAdvertisementController extends Controller
         return view('admin.advertisement_top_view',compact('top_ad_data'));
     }
 
+
+    public function top_ad_update(Request $request)
+    {
+        $top_ad_data = TopAdvertisement::where('id',1)->first();
+
+        if($request->hasFile('top_ad'))
+       {
+        $request->validate([
+            'top_ad'=>'image|mimes:jpg,jpeg,png,gif',
+             ]);
+
+            unlink(public_path('uploads/'.$top_ad_data->top_ad));
+
+            $ext = $request->file('top_ad')->extension();
+            $final_name = 'top_ad'.'.'.$ext;
+
+            $request->file('top_ad')->move(public_path('uploads/'),$final_name);
+            $top_ad_data->top_ad =$final_name;
+       }
+
+           
+        $top_ad_data->top_ad_url = $request->top_ad_url;
+        $top_ad_data->top_ad_status = $request->top_ad_status;
+        $top_ad_data->update();
+        return redirect()->back()->with('success','Data is updated successfully');
+    }
+
 }
